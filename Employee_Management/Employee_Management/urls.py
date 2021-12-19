dc8fe705a4b358e87deae0e760a6dcc41083a7a7
Shivansh Sharma <96285906+ShivanshBharatBhushanSharma@users.noreply.github.com>
2021-12-19 20:43:16 +0530

    Update urls.py

--- Employee_Management/Employee_Management/urls.py
+++ Employee_Management/Employee_Management/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("Employee.urls"))
+    path("employee/", include("Employee.urls"))
 ]
